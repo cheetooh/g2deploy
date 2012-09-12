86ac4587e93f2850efd1817f367339ccaafaccd1
Kelvin Lim <cheetooh@gmail.com>
2012-09-13 02:00:52 +0800

    Update g2deploy.sh

--- g2deploy.sh
+++ g2deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install language-pack-en-base
+apt-get install -y language-pack-en-base
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
