034bb6bc7087668d0614994feb1eb3ef55fb6443
Kelvin Lim <cheetooh@gmail.com>
2012-09-13 01:55:08 +0800

    Update g2deploy.sh

--- g2deploy.sh
+++ g2deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 apt-get update
+apt-get install language-pack-en-base
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
 apt-get install -y aptitude
 aptitude -y full-upgrade
