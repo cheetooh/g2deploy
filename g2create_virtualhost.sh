ce74e3c0edfbfd7dad4b7934c8a37b2ec486653c
root <root@li538-32.members.linode.com>
2012-09-13 12:21:12 +0800

    updates

---
+++ g2create_virtualhost.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# First argument
+# - domain name
+
+# Stackscript ID #1
+source stackscript_1.sh
+
+function apache_virtualhost_g2 {
+	# Configures a VirtualHost
+
+	# $1 - required - the hostname of the virtualhost to create 
+
+	if [ ! -n "$1" ]; then
+		echo "apache_virtualhost() requires the hostname as the first argument"
+		return 1;
+	fi
+
+	if [ -e "/etc/apache2/sites-available/$1" ]; then
+		echo /etc/apache2/sites-available/$1 already exists
+		return;
+	fi
+
+	mkdir -p /srv/www/$1/public_html /srv/www/$1/logs
+
+	echo "<VirtualHost *:80>" > /etc/apache2/sites-available/$1
+	echo "    ServerName $1" >> /etc/apache2/sites-available/$1
+	echo "    DocumentRoot /var/www/$1/" >> /etc/apache2/sites-available/$1
+	echo "    ErrorLog /var/logs/$1/error.log" >> /etc/apache2/sites-available/$1
+    	echo "    CustomLog /var/logs/$1/access.log combined" >> /etc/apache2/sites-available/$1
+        echo "    <Directory /var/www/$1>" >> /etc/apache2/sites-available/$1
+        echo "        Options FollowSymLinks MultiViews" >> /etc/apache2/sites-available/$1
+        echo "        AllowOverride None" >> /etc/apache2/sites-available/$1
+        echo "        Order allow,deny" >> /etc/apache2/sites-available/$1
+        echo "        allow from all" >> /etc/apache2/sites-available/$1
+        echo "    </Directory>" >> /etc/apache2/sites-available/$1
+	echo "</VirtualHost>" >> /etc/apache2/sites-available/$1
+
+	a2ensite $1
+
+	touch /tmp/restart-apache2
+}
+
+apache_virtualhost_g2 $1
+restartServices
--- g2deploy.sh
+++ g2deploy.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# First argument
+# - norestart
+
+function goodstuff_g2 {
+    # Installs the REAL vim, wget, less, and enables color root prompt and the "ll" list long alias
+    aptitude -y install wget vim less
+    sed -i'.bk' -e 's/^#PS1=/PS1=/' /home/g2/.bashrc # enable the colorful root bash prompt
+    sed -i'.bk' -e "s/^#alias ll='ls -l'/alias ll='ls -al'/" /home/g2/.bashrc # enable ll list long alias <3
+}
+
+# Stackscript ID #1
+source stackscript_1.sh
+# Stackscript ID #123
+source stackscript_123.sh
+
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
@@ -9,18 +24,24 @@ apt-get update
 apt-get install -y language-pack-en-base
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
-apt-get install -y aptitude
-aptitude -y full-upgrade
 echo "Asia/Kuala_Lumpur" > /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
 useradd g2 -m -s /bin/bash
-printf "Welcome123\nWelcome123\n" | passwd g2
+printf "Welcome123Juzit\nWelcome123Juzit\n" | passwd g2
 mkdir ~g2/.ssh
 chmod 777 /etc/sudoers
-cat /etc/sudoers | grep -v g2 > /etc/sudoers.tmp
-echo "g2 ALL=(ALL:ALL) ALL" >>/etc/sudoers.tmp
-mv /etc/sudoers.tmp /etc/sudoers
+count=$(grep -i "g2" /etc/sudoers|wc -l)
+if test $count -eq 0
+then
+sed -i'.bk' -e '/# User privilege specification/ a\g2 ALL=(ALL:ALL) ALL' /etc/sudoers
+fi
+count=$(grep -i "pwfeedback" /etc/sudoers|wc -l)
+if test $count -eq 0
+then
+sed -i'.bk' -e "s/env_reset/env_reset,pwfeedback/" /etc/sudoers
+fi
 chmod 0440 /etc/sudoers
+if [ $1 != "norestart" ]; then
 count=$(grep -i "g2deploy" ~/.bashrc|wc -l)
 if test $count -gt 0
 then
@@ -30,5 +51,44 @@ else
 echo "~/g2deploy/g2deploy.sh" >> ~/.bashrc
 shutdown -r now
 fi
+fi
+system_update
+postfix_install_loopback_only
+mysql_install "Welcome123Juzit" && mysql_tune 40
 
+###########################################################
+# PHP
+###########################################################
+php_install_with_apache && php_tune
+sed -i'.bk' 's/^expose_php.*/expose_php = Off/' /etc/php5/apache2/php.ini
+
+###########################################################
+# Apache
+###########################################################
+apache_install && apache_tune 40
+echo "ServerSignature Off" >> /etc/apache2/apache2.conf
+echo "ServerTokens Prod" >> /etc/apache2/apache2.conf
+
+###########################################################
+# SSH
+###########################################################
+system_sshd_permitrootlogin no
+system_sshd_passwordauthentication no
+system_user_add_ssh_key "g2" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmG4HXpZLao+iS8oNH5o+6p35OzyJEJ/GRkyHqUVr1oVapCs0eTXYPL0vCqbXYCpZBM3Hz4XzM4sCBgZiY7ymCC2Y1JVdwEZ/DrcB6YYS4HmzLGmC5gH11/MOfqEBmWaKl/sy4ARJYXggX5yTraFM3w+JWEza2jB0qMIv8EvBxdVR1ItyUK5Mwg8sn4y8YpT+nnXOvLRL89dnkiqo6UR0AhpPWIOrnN9vm0ZnNMy2RVIYGv1vo+fS1KeXxtpLlaBPpKZBBKbveqaqH/ceyPtHaP+sCg94FzUydXv2ZOEnnLCZtxxf49cY9pa6R3Uv9v3Wjvdj8L7YL/CnIqQaPIizn g2@G2MAC.local"
+
+apt-get install -y fail2ban
 apt-get install -y htop
+apt-get install -y php5-suhosin
+apt-get install -y phpmyadmin
+
+goodstuff
+goodstuff_g2
+
+mysql_secure_installation
+passwd g2
+
+system_security_ufw_configure_basic
+ufw allow 1194
+system_lock_user
+
+shutdown -r now
